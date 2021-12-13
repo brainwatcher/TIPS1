9b1d014e3d2867c8e8b26cadfc060d7137d24acf
Zhang Wei <33627072+brainwatcher@users.noreply.github.com>
2021-12-13 12:30:18 +0800

    edit sortedge

--- script/target/SortEdge.m
+++ script/target/SortEdge.m
@@ -1,11 +1,14 @@
-function es = SortEdge_new(e0)
+function es = SortEdge(e0)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 eRemain = e0;
 es = cell(0,1);
 i = 1;
+%%
+[B,BG] = groupcounts(e0(:));
+ends_pool = BG(B==1);
 while size(eRemain,1)>0
-    [es{i},eRemain] = SortEdgeGiveEnd(eRemain,eRemain(1));
+    [es{i},eRemain,ends_pool] = SortEdgeGiveEnd(eRemain,ends_pool);
     i = i+1;
 end
 n = cellfun(@length,es,'un',1);
--- script/target/SortEdgeGiveEnd.m
+++ script/target/SortEdgeGiveEnd.m
@@ -1,24 +1,36 @@
-function [es,eRemain] = SortEdgeGiveEnd(e0,end0)
+function [es,eRemain,ends_pool] = SortEdgeGiveEnd(e0,ends_pool)
 %SORTEDGEGIVEEND Summary of this function goes here
 %   Detailed explanation goes here
-eRemain = e0;
-es=zeros(size(e0));
-[sub1,sub2]=ind2sub(size(e0),find(e0==end0));
-%% first edge of es
-es(1,1)=e0(sub1(1),sub2(1));
-es(1,2)=e0(sub1(1),mod(sub2(1),2)+1);
-eRemain(sub1(1),:)=[];
-%% i from 2
-for i=2:size(e0,1)
-    key = es(i-1,2);
-    if key==end0
-        es(i:end,:) = [];
-        break;
-    else
-    [sub1,sub2]=ind2sub(size(eRemain),find(eRemain==key));
-    es(i,1)= key;
-    es(i,2)= eRemain(sub1(1),mod(sub2(1),2)+1);
+%%
+if isempty(ends_pool)
+    end0 = e0(1,1);
+else
+    end0 = ends_pool(1);
+    ends_pool = setdiff(ends_pool,end0);
+end
+    eRemain = e0;
+    es = zeros(size(e0));
+    [sub1,sub2]=ind2sub(size(e0),find(e0==end0));
+    %% first edge of es
+    es(1,1)=e0(sub1(1),sub2(1));
+    es(1,2)=e0(sub1(1),mod(sub2(1),2)+1);
     eRemain(sub1(1),:)=[];
+    %% i from 2
+    for i=2:size(e0,1)
+        key = es(i-1,2);
+        if key==end0
+            es(i:end,:) = [];
+            break;
+        elseif ismember(key,ends_pool)
+            es(i:end,:) = [];
+            ends_pool = setdiff(ends_pool,key);
+            break;
+        else
+            [sub1,sub2]=ind2sub(size(eRemain),find(eRemain==key));
+            es(i,1)= key;
+            es(i,2)= eRemain(sub1(1),mod(sub2(1),2)+1);
+            eRemain(sub1(1),:)=[];
+        end
     end
 end
 
